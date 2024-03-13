3b56a7f4b8430fbd5ddd2048a9635acf2343e41e
Arthur Gibert <arthurgibert@free.fr>
2024-03-13 10:26:38 +0100

    Ajout du state aircraft

--- src/app/components/aircraft/aircraft.component.ts
+++ src/app/components/aircraft/aircraft.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AircraftService } from 'src/app/services/aircraft.service';
 import { Aircraft } from 'src/app/model/aircraft.model';
+import { Observable, catchError, startWith, map } from 'rxjs';
+import { AppDataState } from 'src/app/state/aircraft.state';
+import { DataStateEnum } from 'src/app/state/aircraft.state';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-aircraft',
@@ -8,8 +12,10 @@ import { Aircraft } from 'src/app/model/aircraft.model';
   styleUrls: ['./aircraft.component.css']
 })
 export class AircraftComponent implements OnInit {
-  aircraftsList : Aircraft[] | null = null;
-  error = null;
+
+  // aircraftsList : Aircraft[] | null = null; OPTION 1 -> tableau d'avions
+  aircrafts$: Observable<AppDataState<Aircraft[]>> | null = null;
+  readonly dataStateEnum = DataStateEnum;
 
   constructor(private aircraftService: AircraftService) { }
 
@@ -17,26 +23,45 @@ export class AircraftComponent implements OnInit {
   }
 
   getAllAircrafts() {
-    this.aircraftService.getAircrafts().subscribe({
-      next: (data) => this.aircraftsList = data,
-      error: (err) => this.error = err.message,
-      complete: () => this.error = null
-    })
+    this.aircrafts$ = this.aircraftService.getAircrafts().pipe(
+      map((data: Aircraft[]) => ({dataState: DataStateEnum.LOADING, data: data})),
+      startWith({dataState: DataStateEnum.LOADED}),
+      catchError(err => of({dataState: DataStateEnum.ERROR, errorMessage: err.message}))
+    );
+    // this.aircraftService.getAircrafts().subscribe({
+    //   next: (data) => this.aircraftsList = data,
+    //   error: (err) => this.error = err.message,
+    //   complete: () => this.error = null
+    // })
   }
 
   getDesignedAircrafts() {
-    this.aircraftService.getDesignedAircrafts().subscribe({
-      next: (data) => this.aircraftsList = data,
-      error: (err) => this.error = err.message,
-      complete: () => this.error = null
-    })
+    this.aircrafts$ = this.aircraftService.getDesignedAircrafts().pipe(
+      map((data: Aircraft[]) => ({dataState: DataStateEnum.LOADING, data: data})),
+      startWith({dataState: DataStateEnum.LOADED}),
+      catchError(err => of({dataState: DataStateEnum.ERROR, errorMessage: err.message}))
+    );
+  //   // this.aircraftService.getDesignedAircrafts().subscribe({
+  //   //   next: (data) => this.aircraftsList = data,
+  //   //   error: (err) => this.error = err.message,
+  //   //   complete: () => this.error = null
+  //   // })
   }
 
   getDevelopmentAircrafts() {
-    this.aircraftService.getDeveloppementAircrafts().subscribe({
-      next: (data) => this.aircraftsList = data,
-      error: (err) => this.error = err.message,
-      complete: () => this.error = null
-    })
+    this.aircrafts$ = this.aircraftService.getDeveloppementAircrafts().pipe(
+      map((data: Aircraft[]) => ({ dataState: DataStateEnum.LOADING, data: data })),
+      startWith({ dataState: DataStateEnum.LOADED }),
+      catchError(err => of({ dataState: DataStateEnum.ERROR, errorMessage: err.message }))
+    );
+  //   this.aircraftService.getDeveloppementAircrafts().subscribe({
+  //     next: (data) => this.aircraftsList = data,
+  //     error: (err) => this.error = err.message,
+  //     complete: () => this.error = null
+  //   })
   }
 }
+
+
+
+
